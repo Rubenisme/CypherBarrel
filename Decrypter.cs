611ff8cf3f76f7ce3e2bb3b29583859971aedb6f
Ruben <rubenisme@gmail.com>
2013-09-08 01:01:09 +0200

    Added encrypter, decrypter and a test sample.

---
+++ Decrypter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace CypherBarrel
+{
+    class Decrypter
+    {
+        private static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(args[0]);
+            string key = null;
+
+            foreach (var line in lines.Skip(1).Select((s, i) =>
+            {
+                if (i % 2 == 0)
+                {
+                    key = s;
+                    return lines[i + 2];
+                }
+                return null;
+            }).Where(line => line != null))
+            {
+                var keypos = 0;
+                foreach (var letter in line)
+                {
+                    if (keypos >= key.Length)
+                    {
+                        key = string.Concat(Enumerable.Reverse(key));
+                        keypos = 0;
+                    }
+                    if (letter == ' ')
+                        Console.Write(' ');
+                    else
+                    {
+                        var newkey = letter - Char.GetNumericValue(key[keypos++]);
+                        if (newkey < 'a')
+                            newkey += 26;
+                        Console.Write((char)newkey);
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
